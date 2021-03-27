be6f637ace08205a8306d0be09ea309385e9d121
sunitee <suniteebajpai2@gmail.com>
2021-03-27 16:55:01 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+// method: "POST" is missing from the object below
+
+function submitData (name, email ) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+            name,
+            email
+        } )
+        
+  } )
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+    console.log(object)
+    document.body.innerHTML = object ["id"];
+  })
+  .catch(function(error) {
+    alert("Bad things! Ragnarők!");
+    console.log(error.message);
+    document.body.innerHTML = error.message
+  }); 
+       
+
+}
+
+
+
